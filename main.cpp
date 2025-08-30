adc4d281e81b93e304cb53d4cee0be2d1e61e816
xSk1d <160499368+xSk1d@users.noreply.github.com>
2025-08-29 18:46:37 -0700

    better version?

--- main.cpp
+++ main.cpp
@@ -1,29 +1,38 @@
 #include <iostream>
+#include <string>
 using i64 = long long;
+
 int main(){
 
-    int people;
-    i64 subtotal, tips, each, tax, total, rem;
+    std::string moneyString;
+    std::cout << "Enter total: ";
+    std::cin >> moneyString;
 
-    std::cout << "How many people?" << std::endl;
-    std::cin >> people;
+    std::string cleaned;
+    for (char character : moneyString) {
+        if (character != ' ') cleaned += character;
+    }
+    std::cout << "Cleaned: " << cleaned << std::endl;
+    auto dot = cleaned.find('.');
+    if (dot == std::string::npos) {
+        std::cout << "No decimal point." << std::endl;
+    } else {
+        std::cout << "Decimal at index: " << dot << std::endl;
+    }
 
-    std::cout << "What's the subtotal?" << std::endl;
-    std::cin >> subtotal;
+    std::string dollars, cents;
+    
+    if (dot == std::string::npos) {
+        dollars = cleaned; 
+        cents = "00";
+    } else {
+        dollars = cleaned.substr(0, dot);
+        cents = cleaned.substr(dot +1);
+    }
 
-    std::cout << "How much tips?" << std::endl;
-    std::cin >> tips;
-
-    std::cout << "How much tax?" << std::endl;
-    std::cin >> tax;
-
-    total = subtotal + tips + tax;
-    each = total / people;
-   
-
-    std::cout << "Each person has to pay: " << each << std::endl;
- 
-
+    if (cents.size() == 0)  cents = "00";
+    else if (cents.size () == 1) cents += "0";
+    else if (cents.size() > 2) cents.resize(2);
 
 return 0;
 }
