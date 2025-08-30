fd4d805b66b2f19b3635b8358468dfa05d38652e
xSk1d <160499368+xSk1d@users.noreply.github.com>
2025-08-29 17:05:40 -0700

    Basic functions

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
-
+using i64 = long long;
 int main(){
+
+    int people;
+    i64 subtotal, tips, each, tax, total, rem;
+
+    std::cout << "How many people?" << std::endl;
+    std::cin >> people;
+
+    std::cout << "What's the subtotal?" << std::endl;
+    std::cin >> subtotal;
+
+    std::cout << "How much tips?" << std::endl;
+    std::cin >> tips;
+
+    std::cout << "How much tax?" << std::endl;
+    std::cin >> tax;
+
+    total = subtotal + tips + tax;
+    each = total / people;
+   
+
+    std::cout << "Each person has to pay: " << each << std::endl;
+ 
+
+
 return 0;
 }
