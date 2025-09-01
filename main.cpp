ce4d02130ddf40ec525414906fc97ce13681cb6b
xSk1d <160499368+xSk1d@users.noreply.github.com>
2025-09-01 16:10:40 -0700

    loops and stuff

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iomanip>
+#include <limits>
 using namespace std;
 
 int main(){
@@ -7,30 +8,37 @@ int main(){
     int people;
 
     cout << "=== Bill Splitter v1 ===\n";
-    cout << "Enter total amount: ";
-    cin >> total;
+    
+    while (true) {
+        cout << "Enter total amount: ";
+        cin >> total;
 
-    if (cin.fail() || total < 0) {
-        cout << "Invalid total amount entered.\n";
-        return 1;
+        if (cin.fail() || total <= 0) {
+            cout << "Invalid total amount. Try again.\n";
+            cin.clear();
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        } else {
+            break;
+        }
     }
 
-    cout << "Enter number of people: ";
-    cin >> people;
+    while (true) {
+        cout << "Enter number of people: ";
+        cin >> people;
 
-    if (cin.fail() || people <= 0) {
-        cout << "Who are you splitting with? Ghosts?\n";
-        return 1;
+        if (cin.fail() || people <= 0) {
+            cout << "Invalid number of people. Try again.\n";
+            cin.clear();
+            cin.ignore(1000, '\n');
+        } else {
+            break;
+        }
     }
    
     double share  = total/people;
 
-    cout << fixed << setprecision(2);
-    cout << "\nTotal: $" << total << "\n";
-    cout << "Each person pays: $" << share << "\n";
+    cout << "\nTotal: $" << fixed << setprecision(2) << total << "\n";
+    cout << "Each person pays: $" << fixed << setprecision(2) << share << "\n";
 
     return 0;
-
-    cout << "git test";
-    cout << "hi";
 }
